ae141f5066eca8ca76dd08266f119802aee79443
a.pshenichnikova <a.pshenichnikova@urfu.me>
2024-01-04 18:43:02 +0500

    logging

--- src/fa.py
+++ src/fa.py
@@ -4,9 +4,24 @@ app = FastAPI()
 import model
 
 def process(input_text):
-    return str(model.analyse(input_text)[0])
+    try:
+        payload =str(model.analyse(input_text)[0])
+        print("model payload: "+payload)
+        return payload
+    except Exception as e:
+        print("It is error on line 12:" +str(e))
+        print("Error inside the model.py")
+        return "model.py error"
+
 
 @app.get("/process_text/{text}")
 async def process_text(text: str):
-    result = process(text)
-    return {"result": result}
+    try:
+        result = process(text)
+        print("Text prosessed normally.")
+        return {"result": result}
+    
+    except Exception as e:
+        print("It is error on line 25:" +str(e))
+        print("Error inside the processing")
+        return {"result": "None"}
