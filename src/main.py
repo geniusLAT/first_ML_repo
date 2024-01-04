98a549e9fedf13f79fbd2c789a305017fb7e1e30
a.pshenichnikova <a.pshenichnikova@urfu.me>
2024-01-04 19:44:25 +0500

    changes in logging

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ import uvicorn
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("fa:app", host="0.0.0.0", port=int(os.environ['PORT']), reload=False, log_level="debug", debug=True, workers=1, limit_concurrency=1, limit_max_requests=1)
+        uvicorn.run("fa:app", host="0.0.0.0", port=int(os.environ['PORT']), reload=False, log_level="debug")
     except Exception as e:
         print("Error on running uvicorn: "+str(e))
